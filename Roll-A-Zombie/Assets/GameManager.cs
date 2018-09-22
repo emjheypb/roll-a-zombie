8cdcab3b940d3bafe01e65f1e89d5a0e8b9f8762
Mariah Baysic <mjpb123@gmail.com>
2018-09-23 00:51:23 +0800

    added C# GameManager

---
+++ Roll-A-Zombie/Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public int selectedZombiePosition;
+	public GameObject selectedZombie;
+	public List<GameObject> zombies;
+	public Vector3 selectedSize;
+	public Vector3 defaultSize;
+
+	void Start() {
+		SelectZombie(zombies[0], 0);
+	}
+
+	void SelectZombie(GameObject newZombie, int newPosition) {
+			selectedZombie = newZombie;
+			selectedZombie.transform.localScale = selectedSize;
+	}
+}
